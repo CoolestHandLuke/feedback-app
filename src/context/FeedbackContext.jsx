e46fce5a46093bf724255c591bd7336aa5f0b9b5
Luke Shea <shealuke@yahoo.com>
2023-08-04 23:42:51 -0500

    Final version of the feedback-app, I might come back later and build out a back end, but for now, it's finished

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -25,15 +25,18 @@ export const FeedbackProvider = ({ children }) => {
     };
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`http://localhost:5000/feedback/${id}`, {
+                method: 'DELETE',
+            });
             setFeedback(feedback.filter((item) => item.id !== id));
         }
     };
 
     // Add feedback
     const addFeedback = async (newFeedback) => {
-        const response = await fetch('/feedback', {
+        const response = await fetch('http://localhost:5000/feedback', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -46,10 +49,18 @@ export const FeedbackProvider = ({ children }) => {
     };
 
     // Update feedback item
-    const updateFeedback = (id, updItem) => {
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem),
+        });
+        const data = await response.json();
         setFeedback(
             feedback.map((item) =>
-                item.id === id ? { ...item, ...updItem } : item
+                item.id === id ? { ...item, ...data } : item
             )
         );
     };
